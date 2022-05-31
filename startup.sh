920a8b3e35b9461307dd339bf6da1cd85ac0401a
v-wei40680 <48787443+v-wei40680@users.noreply.github.com>
2022-06-01 05:49:37 +0800

    Update startup.sh
    random string instead wiki page.
    remove caddy server and cloudflare

--- startup.sh
+++ startup.sh
@@ -7,19 +7,8 @@ envsubst < /root/config.json.tp > /root/config.json
 if [[ -e "/root/html/index.html" ]]; then
     echo "index.html exsit, skip genreate index page"
 else
-    randomurl=$(curl -L 'https://en.wikipedia.org/api/rest_v1/page/random/summary' | jq -r '.content_urls.desktop.page')
-    echo $randomurl
-    curl "$randomurl" -o /root/html/index.html
+    cat /dev/urandom | tr -dc '[:alpha:]' | fold -w ${1:-20} | head -n $((RANDOM/500)) > /root/html/index.html
 fi
 
 # Run V2Ray
-if [[ $TUNNEL_TOKEN ]]; then
-echo 'has tunnel token, run cloudflared tunnel'
-wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -O /root/cloudflared
-chmod +x /root/cloudflared
-# /usr/bin/v2ray -config /root/config.json & /root/cloudflared tunnel --no-autoupdate run --token $TUNNEL_TOKEN & nginx -c /root/nginx.conf -g 'daemon off;'
-v2ray -config /root/config.json & caddy run --config /root/Caddyfile & /root/cloudflared tunnel --no-autoupdate run --token $TUNNEL_TOKEN --protocol http2
-else
-v2ray -config /root/config.json & caddy run --config /root/Caddyfile
-fi
-
+v2ray -config /root/config.json
